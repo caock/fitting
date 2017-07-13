1ee271b7cdc7901878bfb3df1f96c0f61dfa277a
cck <cckit2010@gmail.com>
2017-07-14 01:51:21 +0800

    fix

--- fitting/fitting.cpp
+++ fitting/fitting.cpp
@@ -19,7 +19,7 @@ typedef CGAL::Kd_tree<Traits>      Tree;
 
 std::vector<Point> RandomSphere()
 {
-	const std::size_t PointNum = 1000;
+	const std::size_t PointNum = 10000;
 	// generator for random data points in the square ( (-1,-1), (1,1) )
 	CGAL::Random rnd((unsigned int)time(0));
 	CGAL::Random_points_in_cube_3<Kernel::Point_3> rpit(1.0, rnd);
@@ -29,7 +29,7 @@ std::vector<Point> RandomSphere()
 		tree.insert(Point(*rpit++));
 	}
 	Point center(0, 0, 0);
-	double radius = 0.6;
+	double radius = 0.8;
 	Fuzzy_sphere fs(center, radius, 0.1);
 	std::vector<Point> sphere;
 	tree.search(std::back_inserter(sphere), fs);
@@ -37,46 +37,46 @@ std::vector<Point> RandomSphere()
 	return sphere;
 }
 
-void CheckJacobi(std::function<Eigen::VectorXd(Eigen::VectorXd)> getF, std::function<Eigen::MatrixXd(Eigen::VectorXd)> getJ)
+bool CheckJacobi(std::function<double(Eigen::VectorXd)> getF, std::function<Eigen::RowVectorXd(Eigen::VectorXd)> getJ)
 {
 	const double eps = sqrt(std::numeric_limits<double>::epsilon());
 	Eigen::Vector4d value = Eigen::Vector4d::Random();
-	const std::size_t N = value.size();
-	for (std::size_t i = 0; i < N; i++)
+	const std::size_t Ncase = value.size();
+	Eigen::Vector4d dF = Eigen::Vector4d::Zero();
+	Eigen::Vector4d J = Eigen::Vector4d::Zero();
+	for (std::size_t i = 0; i < Ncase; i++)
 	{
 		Eigen::Vector4d dvalue = value;
 		dvalue[i] += eps;
-		Eigen::VectorXd dF = (getF(dvalue) - getF(value)) / eps;
-		Eigen::VectorXd Ji = getJ(value).col(i);
-		const std::size_t M = dF.size();
-		if (dF.size() != Ji.size() || dF.size() != M)
-		{
-			std::cout << "dF size = " << dF.size() << "\tJi size = " << Ji.size() << std::endl;
-		}
-		else
-		{
-			std::cout << "dF = " << dF.block(0,0,5,1).transpose() << "\nJi = " << Ji.block(0, 0, 5, 1).transpose();
-			double d = (dF - Ji).norm();
-			std::cout << d << std::endl;
-		}
+		dF(i) = (getF(dvalue) - getF(value)) / eps;
+		J(i) = getJ(value)(i);
 	}
+	//std::cout << "dF = " << dF.transpose() << "\nJi = " << J.transpose() << "\nDiff = " << (dF - J).transpose() << std::endl;
+	if ((dF - J).norm() < 0.01)
+		return true;
+	return false;
 }
 
-void GaussNewtonOneStep(std::function<Eigen::VectorXd(Eigen::VectorXd)> getF, std::function<Eigen::MatrixXd(Eigen::VectorXd)> getJ, Eigen::VectorXd& value)
+Eigen::VectorXd GaussNewtonOneStep(std::function<double(Eigen::VectorXd)> getF, std::function<Eigen::RowVectorXd(Eigen::VectorXd)> getJ, Eigen::VectorXd value)
 {
-	CheckJacobi(getF, getJ);
+	if (!CheckJacobi(getF, getJ))
+	{
+		std::cout << "The Jacobi Not Match!" << std::endl;
+		return Eigen::VectorXd::Zero(value.size());
+	}
 
-	Eigen::VectorXd F = getF(value);
-	Eigen::MatrixXd J = getJ(value);
-	Eigen::MatrixXd Jt = J.transpose();
-	Eigen::MatrixXd v = value.transpose();
-
-	value += (Jt*J).inverse() * Jt * F;
+	double F = getF(value);
+	Eigen::RowVectorXd J = getJ(value);
+	Eigen::VectorXd Jt = J.transpose();
+	
+	return (Jt*J).inverse() * Jt * F;
+	//std::cout << "value = " << value << "\ns = " << (Jt*J).inverse() * Jt * F << std::endl;
+	//std::cout << "J = " << J << "\nJt = " << Jt << "\nF = " << F << "\nvalue = " << value << std::endl;
 }
 
 void fittingSphere(std::vector<Point> sphere)
 {
-	Eigen::Vector3d center(0.1, 0.2, -0.3);
+	Eigen::Vector3d center(0.1, 0.2, -0.13);
 	Kernel::Iso_cuboid_3 box = CGAL::bounding_box(sphere.begin(), sphere.end());
 	double radius = sqrt((box.max() - box.min()).squared_length()) * 0.5;
 	//double radius = (box.max().x() - box.min().x()) * 0.5;
@@ -91,40 +91,43 @@ void fittingSphere(std::vector<Point> sphere)
 	//	F(i, 3) = p.z();
 	//	F(i, 4) = 1;
 	//}
-	auto getF = [&sphere](Eigen::VectorXd value)->Eigen::VectorXd
+	auto getF = [&sphere](Eigen::VectorXd value)->double
 	{
 		Eigen::Vector3d z(value[0], value[1], value[2]);
 		double radius = value[3];
-		Eigen::VectorXd F = Eigen::VectorXd::Zero(sphere.size());
+		double f = 0.0;
 		for (std::size_t i = 0; i < sphere.size(); i++)
 		{
 			Eigen::Vector3d p = Eigen::Vector3d(sphere[i].x(), sphere[i].y(), sphere[i].z());
-			F(i) = (p - z).norm() - radius;
-			//F(i) = F(i) * F(i);
+			double d = (p - z).norm() - radius;
+			f += d * d;
 		}
-		return F;
+		return f;
 	};
-	auto getJ = [&sphere](Eigen::VectorXd value)->Eigen::MatrixXd
+	auto getJ = [&sphere](Eigen::VectorXd value)->Eigen::RowVectorXd
 	{
 		Eigen::Vector3d z(value[0], value[1], value[2]);
-		Eigen::MatrixXd J = Eigen::MatrixXd::Zero(sphere.size(), 4);
+		Eigen::VectorXd J = Eigen::VectorXd::Zero(value.size());
 		for (std::size_t i = 0; i < sphere.size(); i++)
 		{
-			Point p = sphere[i];
+			Eigen::Vector3d p = Eigen::Vector3d(sphere[i].x(), sphere[i].y(), sphere[i].z());
+			double radius = value[3];
 			double a = z.x() - p.x();
 			double b = z.y() - p.y();
 			double c = z.z() - p.z();
 			double d = sqrt(a*a + b*b + c*c);
-			J(i, 0) = (z.x() - p.x()) / d;
-			J(i, 1) = (z.y() - p.y()) / d;
-			J(i, 2) = (z.z() - p.z()) / d;
-			J(i, 3) = -1;
+			Eigen::VectorXd Ji = Eigen::VectorXd::Zero(J.size());
+			Ji(0) += (z.x() - p.x()) / d;
+			Ji(1) += (z.y() - p.y()) / d;
+			Ji(2) += (z.z() - p.z()) / d;
+			Ji(3) += -1;
+			J += 2 * ((p - z).norm() - radius) * Ji;
 		}
-		return J;
+		return J.transpose();
 	};
 	auto getEnergy = [&](Eigen::VectorXd value)->double
 	{
-		return getF(value).norm();
+		return getF(value);
 	};
 	Eigen::VectorXd value(4);
 	value.x() = center.x();
@@ -135,8 +138,9 @@ void fittingSphere(std::vector<Point> sphere)
 	std::cout << "begin_center = " << center.transpose() << "\t\t\tradius = " << radius << "\tEnergy = " << beginEnergy << std::endl;
 	for(int i=0; i < 15; i++)
 	{	
-		GaussNewtonOneStep(getF, getJ, value);
-
+		Eigen::VectorXd step = GaussNewtonOneStep(getF, getJ, value);
+		std::cout << "step = " << step.transpose() << std::endl;
+		value += step;
 		center = Eigen::Vector3d(value.x(), value.y(), value.z());
 		radius = value[3];
 		double curEnergy = getEnergy(value);
